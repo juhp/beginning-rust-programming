a7f890d9d35c55cf203d3237887d345a3ddbca67
Jens Petersen <petersen@redhat.com>
2021-09-01 01:29:26 +0800

    ch4 clippy: for index enumerate

--- ch4_hangman/src/main.rs
+++ ch4_hangman/src/main.rs
@@ -5,7 +5,7 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use rand::Rng;
 
-struct Word { 
+struct Word {
     answer: String,
     length: usize,
     correct_count: usize,
@@ -31,22 +31,18 @@ impl CheckLetter for Word {
         let mut count: usize = 0;
         let mut found: bool = false;
         let mut response = String::with_capacity(self.length);
-        let mut index = 0;
-        for letter in self.answer.chars() {
+        for (index,letter) in self.answer.chars().enumerate() {
             if letter == c {
-                found = true; 
+                found = true;
                 count += 1;
                 response.push(c);
             }
-            else {
-                if self.representation.chars().nth(index) != Some('_') {
-                    response.push(self.representation.chars().nth(index).unwrap());
-                }
-                else {
-                response.push('_');
-                }
+            else if self.representation.chars().nth(index) != Some('_') {
+                response.push(self.representation.chars().nth(index).unwrap());
             }
-            index += 1;
+            else {
+                response.push('_');
+            }
         }
         if found {
             println!("Found a ")
@@ -66,7 +62,7 @@ where P: AsRef<Path>, {
 fn read_list(filename: String) -> Vec::<String> {
     let mut v = Vec::<String>::new();
     if let Ok(lines) = read_lines(filename) {
-        for w in lines { 
+        for w in lines {
             let word:String = w.unwrap();
             if word.len() > 4 {
                 v.push(word);
@@ -87,11 +83,11 @@ fn select_word() -> String {
 }
 
 fn main() {
- 
-    let body = vec!["noose".to_string(), "head".to_string(), "neck".to_string(), "torso".to_string(), "left arm".to_string(), 
+
+    let body = vec!["noose".to_string(), "head".to_string(), "neck".to_string(), "torso".to_string(), "left arm".to_string(),
     "right arm".to_string(), "right leg".to_string(), "left leg".to_string(), "left foot".to_string(), "right foot".to_string()];
     let mut body_iter = body.iter();
-    let mut result = select_word();
+    let result = select_word();
     let mut answer = Word {
         length: result.len(),
         representation: String::from_utf8(vec![b'_'; result.len()]).unwrap(),
@@ -105,8 +101,8 @@ fn main() {
         println!("Provide a letter to guess ");
         let mut input = String::new();
         match io::stdin().read_line(&mut input) {
-            Ok(n) => {
-                letter = input.chars().nth(0).unwrap();
+            Ok(_n) => {
+                letter = input.chars().next().unwrap();
                 if answer.check_for_letter(letter) {
                     println!("There is at least one {}, so the word is {}", letter, answer.representation);
                 }
@@ -118,7 +114,7 @@ fn main() {
                     }
                 }
             }
-            Err(error) => {
+            Err(_error) => {
                 println!("Didn't get any input");
             }
         }
