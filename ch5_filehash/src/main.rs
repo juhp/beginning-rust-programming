c6f6fdcd6cbb07962c15f48a582078f6a1c696c7
Jens Petersen <petersen@redhat.com>
2021-09-08 14:02:58 +0800

    ch5 rework: use Channel and add Done msg; only print filename
    - sock_server -> server
    - output now matches sha256sum
    - no longer finishes early etc, like with the unix socket
    
    It would really make more sense to do the sha256 in the thread
    and return the results to the parent: then Done would not be needed...

--- ch5_filehash/src/main.rs
+++ ch5_filehash/src/main.rs
@@ -1,55 +1,51 @@
 use sha2::{Digest, Sha256};
-use std::fs::ReadDir;
-use std::io::Read;
-use std::io::Write;
-use std::os::unix::net::UnixStream;
-// use std::path::Path;
-use std::process;
+use std::ffi::OsString;
+use std::sync::mpsc;
 use std::thread;
 use std::{env, fs};
 
-fn sock_server(mut listener: UnixStream) {
-    loop {
-        let mut response = String::new();
-        let _length = listener.read_to_string(&mut response).unwrap();
-        println!("{}", response);
+enum FileMsg {
+    FileHash(OsString, String),
+    Done
+}
+
+fn server(rx: mpsc::Receiver<FileMsg>) {
+    for msg in rx {
+        match msg {
+            FileMsg::FileHash(file,hash) =>
+                println!("{}  {}", hash, file.to_str().unwrap()),
+            FileMsg::Done => return
+        }
     }
 }
 
-fn get_files(path: &str) -> ReadDir {
-    fs::read_dir(&path).unwrap()
-}
-
 fn read_file(filename: &fs::DirEntry) -> Result<String, ()> {
-    if !filename.path().is_dir() {
-        let contents = match fs::read_to_string(filename.path()) {
-            Ok(contents) => contents,
-            Err(_why) => panic!("{:?}", filename.path()),
-        };
-        Ok(contents)
+    let fpath = filename.path();
+    if !fpath.is_dir() {
+        match fs::read_to_string(fpath) {
+            Ok(content) => Ok(content),
+            Err(err) => {
+                eprintln!("failed to read file {:?}: {:?}", filename.path(), err);
+                Err(())
+            }
+        }
     } else {
         Err(())
     }
 }
 
-fn main() -> Result<(), ()> {
+fn main() {
     let current_dir = String::from(env::current_dir().unwrap().to_str().unwrap());
-    let (side1, mut side2) = UnixStream::pair().unwrap_or_else(|err| {
-        println!("Couldn't create a pair of sockets: {:?}", err);
-        process::exit(-1);
-    });
-    let _serv_handle = thread::spawn(|| sock_server(side1));
-    for file in get_files(&current_dir) {
-        let entry = file.unwrap();
-        if let Ok(file) = read_file(&entry) {
-            let msg = format!(
-                "{} : {:x}\n",
-                entry.path().to_str().unwrap(),
-                Sha256::digest(file.as_bytes())
-            );
-            side2.write_all(&msg.into_bytes()).expect("write failed");
+    let (tx, rx) = mpsc::channel();
+    let server_handle = thread::spawn(|| server(rx));
+    for entry in fs::read_dir(&current_dir).expect("could not read dir") {
+        let entry = entry.unwrap();
+        if let Ok(content) = read_file(&entry) {
+            let path = entry.file_name();
+            let hash = format!("{:x}", Sha256::digest(content.as_bytes()));
+            tx.send(FileMsg::FileHash(path, hash)).expect("write failed");
         }
     }
-
-    Ok(())
+    tx.send(FileMsg::Done).expect("failed to send done");
+    server_handle.join().expect("thread failed")
 }
